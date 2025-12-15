1cb89512d4c177d1ec8a1745e46e360c489374bf
Bijay-Chauhan <bijaychauhan457@gmail.com>
2025-12-16 02:47:40 +0530

    Add files via upload

---
+++ CheckSortedOrNot.java
@@ -0,0 +1,23 @@
+// WAP To check the number is sorted or not
+
+//example
+// arr[] = {15, 2, 3, 5, 10};
+// false
+// arr[] = {1, 2, 3, 5, 10};
+// true
+
+class CheckSortedOrNot {
+    public static boolean checkSorted(int arr[], int i){
+        if(i == arr.length - 1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return checkSorted(arr, i+1);
+    }
+    public static void main(String args[]){
+        int arr[] = {15, 2, 3, 5, 10};
+        System.out.println(checkSorted(arr, 0));
+    }
+}
